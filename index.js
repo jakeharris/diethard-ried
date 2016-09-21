f17a1c2cb54cbc90097a972d8c85dd85a89d77b3
Jake Harris <milieu@javakat.io>
2016-09-21 13:02:38 -0500

    errr

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ diethard.on('ready', function () {
     console.log ('updating from timeout')
     nm.checkForUpdates()
     postAllUpdatesToAllChannels()
-  }.bind(this), timeUntilNoon().bind(this))
+  }.bind(this), timeUntilNoon.call(this))
 })
 
 nm = new NewsManager('./news', {
